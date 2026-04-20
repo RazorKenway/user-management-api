3c8db19e478675baab832c66903e4edcfd343329
H.B. Sandeepa <iit21012@std.uwu.ac.lk>
2026-04-20 16:32:36 +0530

    create user model

---
+++ models/User-model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            min: 0,
+            max: 120
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
